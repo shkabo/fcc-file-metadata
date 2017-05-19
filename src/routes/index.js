c4a1b9f3c7c02628ca1d175ede534a0a296c2d44
Bosko Stupar <bosko.stupar@gmail.com>
2017-05-19 19:03:03 +0200

    route tweak

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
     res.render('content');
 });
 
-router.post('/fileupload', multer({ dest: '../uploads'}).single('fileupload'), (req, res) => {
+router.post('/fileupload', multer().single('fileupload'), (req, res) => {
     res.send({ size: req.file['size']});
 });
 
